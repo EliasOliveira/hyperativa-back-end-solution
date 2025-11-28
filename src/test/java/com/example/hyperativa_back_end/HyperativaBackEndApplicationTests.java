abfa1ad2518a3701ce8a3aa38d6e7ef594176bb0
EliasOliveira <jun.oliveira10@gmail.com>
2025-11-28 15:42:25 -0300

    Initial

---
+++ src/test/java/com/example/hyperativa_back_end/HyperativaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyperativa_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperativaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
